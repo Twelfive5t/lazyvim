251e011796488c50a657d337034388e76a0280ce
liuyijie <liuyijie@fscut.com>
2025-07-18 10:30:49 +0800

    feat: 显示hidden和ignored文件

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true,  -- 显示隐藏文件
+      ignored = true, -- 显示被 .gitignore 忽略的文件
+      sources = {
+        -- <space><space> 搜索文件时
+        files = {
+          hidden = true,    -- 显示隐藏文件
+          ignored = true,  -- 显示被 .gitignore 忽略的文件
+        },
+        -- <leader>fe 或 <space>e 打开侧边栏时
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
+}
