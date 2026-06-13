18ab67159109aae3a9fc6764a3c3ffde059b3aee
Twelfive5t <liuyijieliuyijie@gmail.com>
2026-06-13 12:48:07 +0800

    feat: 禁用 catppuccin 和 flash.nvim 插件

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,6 @@ return {
   -- { "akinsho/bufferline.nvim", enabled = false },
   -- 如果你想禁用其他插件，在这里添加更多行
   -- { "插件作者/插件仓库名", enabled = false },
+  { "catppuccin", enabled = false }, -- 禁用 catppuccin 主题插件
+  { "flash.nvim", enabled = false }, -- 禁用 flash.nvim 插件
 }
