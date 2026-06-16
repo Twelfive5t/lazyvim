2872d61e51c03ce8e72593a31864b06ee3158e7c
liuyijie <liuyijie@fscut.com>
2026-06-16 13:47:12 +0800

    feat: 优化 autocmds.lua 中 im-select 的条件检查

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,16 +7,22 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
-  callback = function()
-    vim.fn.jobstart({ "im-select", "1033" }, {
-      on_exit = function()
-        vim.schedule(function()
-          vim.fn.jobstart({ "im-select", "2052" }, {
-            detach = true,
-          })
-        end)
-      end,
-    })
-  end,
-})
+local im_select = vim.fn.executable("im-select") == 1 and "im-select"
+  or vim.fn.executable("im-select.exe") == 1 and "im-select.exe"
+  or nil
+
+if im_select then
+  vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+    callback = function()
+      vim.fn.jobstart({ im_select, "1033" }, {
+        on_exit = function()
+          vim.schedule(function()
+            vim.fn.jobstart({ im_select, "2052" }, {
+              detach = true,
+            })
+          end)
+        end,
+      })
+    end,
+  })
+end
